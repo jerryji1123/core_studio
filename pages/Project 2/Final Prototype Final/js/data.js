567863d91cf37d69ed501c92ca92605cfe7a05d9
jerryji1123 <jiz423@newschool.edu>
2020-04-08 23:35:17 -0400

    Project 2 final

---
+++ pages/Project 2/Final Prototype Final/js/data.js
@@ -0,0 +1,166 @@
+var data1 = [
+    {
+        uid: 0,
+        name: 'Caris LeVert',
+        png: "./images/1.png",
+        pts: 30,
+        reb: 4,
+        ast: 8
+    },
+    {
+        uid: 1,
+        name: 'Joe Harris',
+        png: "./images/2.png",
+        pts: 17,
+        reb: 9,
+        ast: 1
+    },
+    {
+        uid: 2,
+        name: 'Garrett Temple',
+        png: "./images/3.png",
+        pts: 16,
+        reb: 6,
+        ast: 2
+    },
+    {
+        uid: 3,
+        name: 'Taurean Prince',
+        png: "./images/4.png",
+        pts: 12,
+        reb: 6,
+        ast: 1
+    },
+    {
+        uid: 4,
+        name: 'Rodions Kurucs',
+        png: "./images/5.png",
+        pts: 12,
+        reb: 5,
+        ast: 0
+    },
+    {
+        uid: 5,
+        name: 'DeAndre Jordan',
+        png: "./images/6.png",
+        pts: 10,
+        reb: 7,
+        ast: 0
+    },
+    {
+        uid: 6,
+        name: 'Wilson Chandler',
+        png: "./images/7.png",
+        pts: 9,
+        reb: 3,
+        ast: 7
+    },
+    {
+        uid: 7,
+        name: 'Dzanan Musa',
+        png: "./images/8.png",
+        pts: 9,
+        reb: 4,
+        ast: 0
+    },
+    {
+        uid: 8,
+        name: 'Spencer Dinwiddie',
+        png: "./images/9.png",
+        pts: 8,
+        reb: 5,
+        ast: 4
+    },
+    {
+        uid: 9,
+        name: 'Chris Chiozza',
+        png: "./images/10.png",
+        pts: 7,
+        reb: 1,
+        ast: 1
+    },
+    {
+        uid: 10,
+        name: 'Jeremiah Martin',
+        png: "./images/11.png",
+        pts: 2,
+        reb: 0,
+        ast: 1
+    },
+    {
+        uid: 11,
+        name: 'Theo Pinson',
+        png: "./images/12.png",
+        pts: 0,
+        reb: 2,
+        ast: 5
+    }
+];
+var data2 = [
+    {
+        uid: 0,
+        name: 'D’Angelo Russell',
+        png: "./images/b1.png",
+        pts: 17,
+        reb: 3,
+        ast: 3
+    },
+    {
+        uid: 1,
+        name: 'Jordan Poole',
+        png: "./images/b2.png",
+        pts: 16,
+        reb: 2,
+        ast: 4
+    },
+    {
+        uid: 2,
+        name: 'Eric Paschall',
+        png: "./images/b3.png",
+        pts: 15,
+        reb: 5,
+        ast: 2
+    }, {
+        uid: 3,
+        name: 'Kevon Looney',
+        png: "./images/b4.png",
+        pts: 11,
+        reb: 5,
+        ast: 3
+    }, {
+        uid: 4,
+        name: 'Marquese Chriss',
+        png: "./images/b5.png",
+        pts: 8,
+        reb: 7,
+        ast: 0
+    }, {
+        uid: 5,
+        name: 'Jacob Evans',
+        png: "./images/b6.png",
+        pts: 8,
+        reb: 4,
+        ast: 4
+    }, {
+        uid: 6,
+        name: 'Damion Lee',
+        png: "./images/b7.png",
+        pts: 7,
+        reb: 4,
+        ast: 1
+    }, {
+        uid: 7,
+        name: 'Draymond Green',
+        png: "./images/b8.png",
+        pts: 5,
+        reb: 3,
+        ast: 2
+    }, {
+        uid: 8,
+        name: 'Omari Spellman',
+        png: "./images/b9.png",
+        pts: 1,
+        reb: 1,
+        ast: 1
+    },
+]
